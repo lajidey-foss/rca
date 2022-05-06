5c5559f564bf75dcc7c75fcf35f01ff38855f90e
estackhub <spryng.managed@gmail.com>
2022-05-06 02:37:42 +0000

    Sub version better refresh

--- rca/rcapp/api/invoice.js
+++ rca/rcapp/api/invoice.js
@@ -7,7 +7,7 @@ frappe.ui.form.on("Sales Invoice", {
 	},
 	refresh(frm){
 		//console.table(frm.doc);
-		if(frm.doc.return_against != null && frm.doc.is_return && frm.doc.name == 'new-sales-invoice-1' ){
+		if(frm.doc.return_against != null && frm.doc.is_return && frm.is_new() ){
 			//console.log('return against in refresh -- got here: '+ frm.doc.docstatus);
 			cur_frm.set_value("entry_type", " ");	
 		}
